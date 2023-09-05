ab950b0962ca5921d05f0c6fcbceb47424e36430
kankanzhou <zhoukanweekly@outlook.com>
2023-09-05 00:14:46 -0700

    update

--- Demo1/src/Day7/Demo1.java
+++ Demo1/src/Day7/Demo1.java
@@ -5,11 +5,16 @@ package Day7;
  * @Date: 2023-09-04 10:37 p.m.
  */
 public class Demo1 {
+    public String method(StringBuilder s) {
+        return s + "";
+    }
     public static void main(String[] args) {
         StringBuilder s = new StringBuilder();
         s.append(123).append("kanzhou");
         System.out.println(s);
         Long i = 100L;
         System.out.println(i.longValue());
+        String s1 = new Demo1().method(new StringBuilder(100));
+        System.out.println(s1);
     }
 }
